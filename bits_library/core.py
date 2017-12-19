a768cf673193d92a59a623fe430e9660bd6014ec
Aditya Asgaonkar <aditya.asgaonkar@gmail.com>
2017-12-19 12:05:42 +0530

    Updated UI, core.py

--- bits_library/core.py
+++ bits_library/core.py
@@ -1,5 +1,19 @@
 import pandas as pd
+import os
+import time
+
+data_file = 'data/example.csv'
+data = None
+last_read = 0
 
 def getEntries():
-    data = pd.read_csv('data/example.csv')
+    global data
+    global last_read
+    print("Last Read:\t", last_read)
+    print("Last Modified:\t", os.path.getmtime(data_file))
+    if os.path.getmtime(data_file)<last_read and data is not None:
+        return data
+    print("[*] Reading file:", data_file)
+    data = pd.read_csv(data_file)
+    last_read = time.time()
     return data
